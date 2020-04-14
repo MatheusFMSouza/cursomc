816e0ffbf74d1ff39f3cfe5a29991ed642a2ef62
Matheus Fideles <matheuss.fideles@hotmail.com>
2020-04-14 17:47:35 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/matheus/cursomc/CursoMcApplication.java
+++ src/main/java/br/com/matheus/cursomc/CursoMcApplication.java
@@ -1,13 +1,28 @@
 package br.com.matheus.cursomc;
 
+import br.com.matheus.cursomc.domain.Categoria;
+import br.com.matheus.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoMcApplication {
+public class CursoMcApplication implements CommandLineRunner {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoMcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
