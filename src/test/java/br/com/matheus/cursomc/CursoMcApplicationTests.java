54e93fa1e744402830be679e88bc2c3ebc8bc795
Matheus Fideles <matheuss.fideles@hotmail.com>
2020-04-14 15:40:10 -0300

    Projeto criado

---
+++ src/test/java/br/com/matheus/cursomc/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
