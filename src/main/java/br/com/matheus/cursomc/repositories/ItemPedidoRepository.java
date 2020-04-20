fe4d40291f78597049f572243e8ee19839a3dc7e
Matheus Fideles <matheuss.fideles@hotmail.com>
2020-04-19 23:55:09 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/br/com/matheus/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package br.com.matheus.cursomc.repositories;
+
+import br.com.matheus.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+
+}
