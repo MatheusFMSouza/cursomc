2f7f013c125e4a34ba7e12787605b36749607e56
Matheus Fideles <matheuss.fideles@hotmail.com>
2020-04-23 21:58:55 -0300

    Correcao UPDATE em CategoriaService e construtor de Pagamento

--- src/main/java/br/com/matheus/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/matheus/cursomc/services/CategoriaService.java
@@ -33,9 +33,11 @@ public class CategoriaService {
         return repo.save(obj);
     }
 
+
     public Categoria update(Categoria obj) {
-        find(obj.getId());
-        return repo.save(obj);
+        Categoria newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
     }
 
     public void delete(Integer id) {
@@ -57,9 +59,11 @@ public class CategoriaService {
         return repo.findAll(pageRequest);
     }
 
-    public Categoria fromDTO(CategoriaDTO objDTO){
-        return new Categoria(objDTO.getId(),objDTO.getNome());
+    public Categoria fromDTO(CategoriaDTO objDTO) {
+        return new Categoria(objDTO.getId(), objDTO.getNome());
     }
 
-
+    private void updateData(Categoria newObj, Categoria obj) {
+        newObj.setNome(obj.getNome());
+    }
 }
